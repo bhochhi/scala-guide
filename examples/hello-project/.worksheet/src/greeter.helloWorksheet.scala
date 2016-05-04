9df890e3a6ad535612ace54d7399870d5195fae1
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-05-04 16:44:22 -0500

    cleanup

--- examples/hello-project/.worksheet/src/greeter.helloWorksheet.scala
+++
@@ -1,7 +0,0 @@
-package greeter
-
-object helloWorksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(84); 
-  println("Welcome to the Scala worksheet");$skip(20); 
-  println("Hellos");$skip(14); 
-  val d = 110;System.out.println("""d  : Int = """ + $show(d ))}
-}
